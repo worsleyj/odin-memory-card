fdc5a8927e3bfe347c6a5a0d2fecde2e71198d3c
worsleyj <josephworsley23@gmail.com>
2024-10-01 15:02:29 -0400

    add score increase on unique card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import joe from "../assets/joe.jpg";
 import "../styles/Card.css";
 
 export default function Card() {
-  let hasBeenClicked = "false";
+  const [clicked, setClicked] = useState("false");
+  const [score, setScore] = useState(0);
   function selectCard() {
-    console.log("selected card");
+    if (clicked === "false") {
+      setClicked("true");
+      setScore(score + 1);
+    }
   }
   return (
     <>
       <div onClick={selectCard} className="card-container">
         <img src={joe} alt="joe" className="card"></img>
         <h2>Joe</h2>
-        <h6>{hasBeenClicked}</h6>
+        <h6>{score}</h6>
+        <h6>{clicked}</h6>
       </div>
     </>
   );
