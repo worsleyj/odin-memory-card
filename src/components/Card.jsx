a1ea6479416d3c25f880579597e813900a923a88
worsleyj <josephworsley23@gmail.com>
2024-10-02 13:30:03 -0400

    prevent duplicate cards from being generated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,29 @@
 import { useState, useEffect } from "react";
 import "../styles/Card.css";
 
+const uniqueNumbers = [];
+
 export default function Card() {
   const [image, setImage] = useState("");
   const [name, setName] = useState("Pokemon");
   const [clicked, setClicked] = useState("false");
   const [score, setScore] = useState(0);
-  const pokemonNumber = Math.floor(Math.random() * 150 + 1);
+
   useEffect(() => {
     async function fetchPokemon() {
       let data = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/" + pokemonNumber
+        "https://pokeapi.co/api/v2/pokemon/" + generatePokemonNumber()
       );
       let pokemon = await data.json();
       setImage(pokemon.sprites.front_default);
       setName(pokemon.name);
     }
     fetchPokemon();
+    return () => {
+      uniqueNumbers.pop();
+    };
   }, []);
 
-  function selectCard() {
-    if (clicked === "false") {
-      setClicked("true");
-      setScore(score + 1);
-    } else {
-      alert("already clicked!");
-    }
-  }
   return (
     <>
       <div onClick={selectCard} className="card-container">
@@ -37,4 +34,22 @@ export default function Card() {
       </div>
     </>
   );
+}
+
+function generatePokemonNumber() {
+  const pokemonNumber = Math.floor(Math.random() * 150 + 1);
+  if (uniqueNumbers.includes(pokemonNumber)) {
+    return generatePokemonNumber();
+  }
+  uniqueNumbers.push(pokemonNumber);
+  return pokemonNumber;
+}
+
+function selectCard() {
+  if (clicked === "false") {
+    setClicked("true");
+    setScore(score + 1);
+  } else {
+    alert("already clicked!");
+  }
 }
