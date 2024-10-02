03fdc3e9734e1a70988f55dfb669ed9c50b5428e
worsleyj <josephworsley23@gmail.com>
2024-10-02 17:02:30 -0400

    add hover animations to cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header({ score, highScore }) {
   return (
     <>
       <div className="header-container">
+        <h1 className="title">Poké-mnemonics!</h1>
         <h1>High Score: {highScore}</h1>
         <h2>Current Score: {score}</h2>
       </div>
